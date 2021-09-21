b831ead108efc0cedb5043d68c3d177b2809ae11
Jakob Lumpkin <lumpkinjakobr@gmail.com>
2021-09-21 08:31:24 -0500

    Updated LAMP script with Github file

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -30,5 +30,7 @@ service mysqld start
 mysql -u root -e "SHOW DATABASES";
 
 # Download Starter Content
-
+cd /vvagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/jakoblumpkin/vagrant/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/jakoblumpkin/vagrant/master/files/info.php
 service httpd restart
